16f9135aa258e85ae29f61f7b2a3f2e35a616239
Arne Rudek <mail@arne-rudek.de>
2021-09-27 17:23:54 +0200

    Initial Commit

---
+++ TwoPlayerGame/TwoPlayerGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoPlayerGameApp.swift
+//  TwoPlayerGame
+//
+//  Created by Arne Rudek on 27.09.21.
+//
+
+import SwiftUI
+
+@main
+struct TwoPlayerGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
